7fa52cb87d765bfd4f5fd458339669c6a0f61174
steizy <steizym.fornica2@itjo.edu.ve>
2026-05-18 20:09:52 -0400

    Resolver conflicto local en App

--- frontend/BlueTrade/src/App.jsx
+++ frontend/BlueTrade/src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   useEffect(() => {
     async function loadItems() {
       const res = await getItems();
-      
       setInfo(res.data);
       
       console.log("Primer item:", res.data[0].name); 
@@ -27,9 +26,10 @@ function App() {
           <h1>Lista de Items</h1>
           <p>
             {info.map((item) => (
-              <li key={item.id}>
-                <strong>{item.name}</strong> - ${item.price}
-                <p>{item.description}</p>
+              <li key={item.id} className="p-4 bg-gray-50 border border-gray-200 rounded-lg hover:shadow-md transition-shadow">
+                <strong className="text-lg text-gray-800">{item.name}</strong> 
+                <span className="text-green-600 font-bold ml-2">- ${item.price}</span>
+               <p className="text-gray-600 mt-2">{item.description}</p>
               </li>
             ))}
             </p>
@@ -41,4 +41,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
